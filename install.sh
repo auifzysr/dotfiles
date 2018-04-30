86d21ac808e9ca611e95057d21d8cf9b47b33b69
auifzysr <auifzysr@gmail.com>
2018-04-30 12:30:59 +0900

    add powerlevel9k theme

--- install.sh
+++ install.sh
@@ -1,13 +1,20 @@
 #!/bin/bash -xe
 
+# .tmux
 cd ~
 ln -s -f .tmux/.tmux.conf
 cp .tmux/.tmux.conf.local .
 
+# oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# powerline fonts
 if [ -f /etc/os_release ] && [ $(grep -E "[Dd]ebian" /etc/os_release) ]; then
     sudo apt install fonts-powerline
 fi
+
+# powerlevel9k theme for oh-my-zsh
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
 
 exit 0
