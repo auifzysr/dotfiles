7011188e5d6e6134ef6b89126ba3d93d3c4a013e
auifzysr <auifzysr@gmail.com>
2021-01-03 01:19:52 +0900

    fix missing parts

--- install.sh
+++ install.sh
@@ -12,10 +12,13 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp .tmux.conf ~
 cp .tmux.conf.local ~
 
-sudo apt -y install zsh
+sudo apt update && sudo apt -y install zsh tmux
 
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# apply zshrc
+cp .zshrc ~/
 
 # powerline fonts
 if [ -f /etc/os_release ] && [ $(grep -E "[Dd]ebian|Ubuntu" /etc/os_release) ]; then
@@ -25,7 +28,10 @@ fi
 # powerlevel9k theme for oh-my-zsh
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
-git clone https://github.com/caiogondim/bullet-train.zsh
-cp bullet-train.zsh/bullet-train.zsh-theme ~/.oh-my-zsh/themes/
+# git clone https://github.com/caiogondim/bullet-train.zsh
+# cp bullet-train.zsh/bullet-train.zsh-theme ~/.oh-my-zsh/themes/
+cp powerlevel9k.zsh-theme ~/.oh-my-zsh/custom/themes/powerlevel9k/
 
 cd ${ORIGINAL_DIR}
+
+# TODO: setup golang
